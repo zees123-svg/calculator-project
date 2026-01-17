a877379244fc147780a46bc35a63a24d43eb881d
Zeeshan Ramzan <versatilezeeshan1708@gmail.com>
2026-01-17 11:19:05 +0500

    Refactor calculator input handling in script.js

--- Project3/assets/js/script.js
+++ Project3/assets/js/script.js
@@ -1,25 +1,3 @@
-// let string = "";
-// let buttons = document.querySelectorAll(".button");
-
-// Array.from(buttons).forEach((button) => {
-//     button.addEventListener('click', (e) => {
-//         if (e.target.innerHTML === '=') {
-//             string = eval(string);
-//             document.querySelector("input").value = string;
-//         }
-//         else if (e.target.innerHTML === 'C') {
-//             string = "";
-//             document.querySelector("input").value = string;
-//         }
-//         else {
-//             console.log(e.target);
-//             string = string + e.target.innerHTML;
-//             document.querySelector("input").value = string;
-//         }
-//     })
-// })
-
-
 let string = "";
 let buttons = document.querySelectorAll(".button");
 let input = document.querySelector("input");
@@ -62,4 +40,5 @@ Array.from(buttons).forEach((button) => {
             input.value = string;
         }
     });
+
 });
